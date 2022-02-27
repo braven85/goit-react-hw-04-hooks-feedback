da5c00db64ecf74e9404b14533b320b911c4b3dd
Radek <radekie@wp.pl>
2022-02-27 19:55:24 +0100

    repo update

--- src/components/Repository.jsx
+++ src/components/Repository.jsx
@@ -1,10 +1,12 @@
 export const Repository = () => {
-  return <>
-    <a href="https://github.com/braven85/goit-react-hw-02-feedback">
-      Repozytorium kodu na GitHub
-    </a>
-    <a href="https://braven85.github.io/goit-react-hw-02-feedback/">
-      GitHub Pages
-    </a>
-  </>
+  return (
+    <>
+      <a href="https://github.com/braven85/goit-react-hw-04-hooks-feedback">
+        Repozytorium kodu na GitHub
+      </a>
+      <a href="https://braven85.github.io/goit-react-hw-04-hooks-feedback/">
+        GitHub Pages
+      </a>
+    </>
+  );
 }
